fb63e95d7a05c6b2a418a3a04adba08bf1976c68
austintyrome <austintyrome@gmail.com>
2024-06-03 18:53:21 -0600

    Test Push

--- index.js
+++ index.js
@@ -2,13 +2,49 @@
 const inquirer = require('inquirer');
 const fs = require('fs')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    },
+    {
+        type: 'input',
+        name: 'animal',
+        message: 'What is your favorite animal'
+    },
+    {
+        type: 'choices',
+        name: 'color',
+        message: 'Which of these colors is better?',
+        choices: ['Purple', 'Orange', 'Green', 'Blue']
+    }
+
+];
+
+// inquirer.prompt(questions)
+//     .then(answers => {
+//         console.log('Answers:', answers);
+//     })
+//     .catch(error => {
+//         console.error('Error:', error);
+//     });
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, questions) {
+    fs.writeFile('README.md', content, (err) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log('README.md file has been created successfully.');
+    });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,60 @@
+const fs = require('fs');
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-
+    // Define a mapping of licenses to their badge URLs
+    const licenseBadges = {
+      'MIT': 'https://img.shields.io/badge/License-MIT-yellow.svg',
+      'Apache-2.0': 'https://img.shields.io/badge/License-Apache%202.0-blue.svg',
+      'GPL-3.0': 'https://img.shields.io/badge/License-GPLv3-blue.svg',
+      'BSD-3-Clause': 'https://img.shields.io/badge/License-BSD%203--Clause-blue.svg',
+      'ISC': 'https://img.shields.io/badge/License-ISC-blue.svg',
+      'MPL-2.0': 'https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg',
+      'AGPL-3.0': 'https://img.shields.io/badge/License-AGPL%20v3-blue.svg',
+      // Add more licenses and badges as needed
+    };
+  
+    // Return the badge URL if the license is found, otherwise return an empty string
+    return licenseBadges[license] || '';
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-
+    try {
+      // Read the README file
+      const readmeContent = fs.readFileSync(filePath, 'utf-8');
+      
+      // Define a regular expression to find a license link
+      const licenseLinkRegex = /\[.*?license.*?\]\((.*?)\)/i;
+      const match = readmeContent.match(licenseLinkRegex);
+      
+      // If a match is found, return the license link, otherwise return an empty string
+      return match ? match[1].trim() : '';
+    } catch (error) {
+      console.error(`Error reading file: ${error.message}`);
+      return '';
+    }
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-
-}
+    try {
+      // Read the README file
+      const readmeContent = fs.readFileSync(filePath, 'utf-8');
+      
+      // Define a regular expression to find the License section
+      const licenseRegex = /## License[\s\S]*?(?=##|$)/i;
+      const match = readmeContent.match(licenseRegex);
+      
+      // If a match is found, return the License section, otherwise return an empty string
+      return match ? match[0].trim() : '';
+    } catch (error) {
+      console.error(`Error reading file: ${error.message}`);
+      return '';
+    }
+  }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
